0114456f84da214d44b610db45752b3f61f8d899
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-03-26 07:42:22 +0000

    PR changes

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -5,7 +5,7 @@ import { CommentInfo } from '../CommentInfo/CommentInfo';
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
-      <CommentInfo comment={comment} key={comment.id} />
+      <CommentInfo key={comment.id} comment={comment} />
     ))}
   </div>
 );
